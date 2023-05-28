3b9ab1d41fcc63b8d012c66929bd753d233bb0ba
tom-2810 <tom.poortman@hva.nl>
2023-05-28 12:40:11 +0200

    project template start

--- src/routes/partners-page.js
+++ src/routes/partners-page.js
@@ -1,6 +1,17 @@
 import express from 'express'
 
+import { fetchJson, postJson } from '../helpers/fetchWrapper.js'
+
 const partnersPage = express.Router()
+
+// base API calls
+const baseUrl = 'https://api.vervoerregio-amsterdam.fdnd.nl/api/v1'
+const faviconAPI = 'https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url='
+
+
+// base API urls with endpoint and/or query params
+const projectsData = await fetchJson(`${baseUrl}/urls?first=5`).then((data) => data);
+
 
 const options = {
 	path: '/partners',
@@ -9,9 +20,13 @@ const options = {
 	styles: ['/pages/partners.css'],
 	scripts: ['/components/partners.js'],
 	hostClass: 'partners',
-	messages: []
+	messages: [],
+	projectsData,
+	faviconAPI
 }
 
 partnersPage.get(options.path, (request, response) => response.render('index', options))
+
+console.log(projectsData)
 
 export default partnersPage
