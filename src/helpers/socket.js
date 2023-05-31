682a56346b021c3b66c36066aa69e3b07b150fd0
Luuk <luukbrauckmann@gmail.com>
2023-05-31 11:00:09 +0200

    NIeuwe svg toegevoegd

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -23,10 +23,15 @@ export const departureTimer = (socket) => {
 }
 
 export const assignPlayerToSeat = (player) => {
+	// Check if player is already checked in
 	const alreadyAssigned = seats.find(seat => seat.player === player.id)
 	if (alreadyAssigned) return
+
+	// Assign player to seat
 	const seat = seats.findLast(seat => seat.player === null)
 	if (seat) seat.player = player.id
+
+	// Check if all seats are assigned
 	const avalableSeats = seats.filter(seat => seat.player !== null)
 	if (avalableSeats.length === 18) departureTime = 1
 }
