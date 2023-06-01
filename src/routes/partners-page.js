a37ddfdc33e2425063cedc45a06744edfc336592
tom-2810 <tom.poortman@hva.nl>
2023-06-01 12:32:05 +0200

    UPDATE filter and sorting

--- src/routes/partners-page.js
+++ src/routes/partners-page.js
@@ -30,10 +30,56 @@ const getPartners = () => get("websites")
 
 partnersPage.get(options.path, async (request, response) => {
 	const partnersResponse = await getPartners()
+
+	options["partners"] = partnersResponse.websites
+	
 	let { filterPartner } = request.query
+
+	const sortFields = [
+		{ label: 'Partner', field: 'titel' },
+		{ label: 'Datum', field:  'createdAt'}
+	]
+
+	const sortDirections = [
+		{ label: 'Oplopend', field: 'ASC' },
+		{ label: 'Aflopend', field: 'DESC' }
+	]
+
+	  // gets sort value from url query param
+	  let { sortField } = request.query
+	  let { sortDirection } = request.query
+	  // parses the value so it can be accessed as JSON
+	  if (sortField) {
+		sortField = JSON.parse(sortField)
+	  } else {
+		sortField = sortFields[0]
+	  }
+	  if (sortDirection) {
+		sortDirection = JSON.parse(sortDirection)
+	  } else {
+		sortDirection = sortDirections[0]
+	  }
+	  // clones allProjectsData.urls so .reverse() function won't affect the original
+	  let projects = [...projectsData.urls];
+	  // gets the underlaying website titel and maps it straight to url as titel
+	  projects = projects.map((project) => ({ ...project, titel: project.website.titel }))
+	  // sorts every url by given sort option field of cloned allProjectsData.urls
+	  projects = projects.sort((a, b) => a[sortField.field].localeCompare(b[sortField.field]))
+	  //checks for sorting direction, if direction is DESC, reverse cloned urls ones
+	  if (sortDirection.field === 'DESC') projects.reverse()
+
+	  console.log(sortField)
 
 	options["partners"] = partnersResponse.websites
 	options['filterPartner'] = filterPartner
+
+	options["sortFields"] = sortFields
+	options["currentSortField"] = sortField
+
+	options["sortDirections"] = sortDirections
+	options["currentSortDirection"] = sortDirection
+
+	options["projectsData"] = projects
 
 	return response.render('index', options)
 })
